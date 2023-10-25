bd4b3dd7a42ac5188a9ec63e2e6e3377fac4e661
Renzo <edrickventura1996@gmail.com>
2023-10-25 17:43:48 +1100

    init

---
+++ scenes/character.gd
@@ -0,0 +1,47 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+		if direction == 1: 
+			$"Tilemap-characters".scale.x = -1 
+		else: 
+			$"Tilemap-characters".scale.x = 1 
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if velocity.x != 0:
+		$AnimationPlayer.play("walk")
+	else:
+		$AnimationPlayer.stop()
+
+	if velocity.y != 0:
+		$AnimationPlayer.stop()
+	move_and_slide()
+	
+
+
+func _on_area_2d_body_entered(body):
+	print(body.name)
+	if body.name == "spikes":
+		get_tree().reload_current_scene()
+	if body.name == "leveldetails":
+		get_tree().reload_current_scene()
